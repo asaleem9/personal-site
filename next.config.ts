edc05d5d853228271c2ab38c76c23b1ff3a398cb
Ali <alisaleem4412@gmail.com>
2026-01-16 18:30:11 -0800

    Added Medium CDN to allowed image domains (#9)
    Co-authored-by: Ali <alisaleem4412@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn-images-1.medium.com',
+      },
     ],
   },
 };
